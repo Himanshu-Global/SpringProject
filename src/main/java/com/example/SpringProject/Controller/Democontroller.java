6e45d76a206d8aba431f4d41f3ff791ad20dd6eb
Himanshu Semwal <himanshu.semwal@globallogic.com>
2024-06-06 14:23:55 +0530

    second commit

--- src/main/java/com/example/SpringProject/Controller/Democontroller.java
+++ src/main/java/com/example/SpringProject/Controller/Democontroller.java
@@ -3,11 +3,14 @@ package com.example.SpringProject.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.SQLOutput;
+
 @RestController
 public class Democontroller {
     @GetMapping("/hello")
     public void hello(){
         System.out.println("hello");
+        System.out.println("Hello from feature 1");
     }
 
 }
